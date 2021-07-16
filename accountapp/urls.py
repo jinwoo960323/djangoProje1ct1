d1dc1ccf014fde93eab33093d4996e9c242019ef
jinwoo kim <jinwoo960323@likelion.org>
2021-07-16 11:20:34 +0900

    7/16 3 class commit 2

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
          name='login'),
     path('logout/',LogoutView.as_view(),name='logout'),
 
-    path('create/' , AccountCreateView.as_view(), name='create')
+    path('create/' , AccountCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', AccountCreateView.as_view(), name='detail'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -31,3 +31,13 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
+
+class DetailView:
+    pass
+
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
