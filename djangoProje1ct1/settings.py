06977ac2c4b4f5dbb0bbee0a6f3f5b2021d407f8
jinwoo kim <jinwoo960323@likelion.org>
2021-07-23 10:14:56 +0900

    7/23 3 class commit 2

--- djangoProje1ct1/settings.py
+++ djangoProje1ct1/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'accountapp'
+    'profileapp'
 ]
 
 MIDDLEWARE = [
@@ -134,14 +135,17 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
