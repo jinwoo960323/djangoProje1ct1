fef7b946a7c086e0425f432f8c314e79db56a050
jinwoo kim <jinwoo960323@likelion.org>
2021-07-23 10:45:39 +0900

    7/23 3 class commit 3

--- djangoProje1ct1/settings.py
+++ djangoProje1ct1/settings.py
@@ -55,8 +55,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
-    'accountapp'
-    'profileapp'
+    'accountapp',
+    'profileapp',
 ]
 
 MIDDLEWARE = [
