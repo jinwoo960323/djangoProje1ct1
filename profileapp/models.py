8bbe6bdfb7be8f8129137d2bb38c316be1d03685
jinwoo kim <jinwoo960323@likelion.org>
2021-07-28 15:36:20 +0900

    7/28 3 class commit 1

--- profileapp/models.py
+++ profileapp/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,
-                                related_name='profile')
+                                related_name='prof')
 
     image = models.ImageField(upload_to='profile/', null=True)
     nickname = models.CharField(max_length=30, unique=True, null=True)
