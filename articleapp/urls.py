1b2433acfa6c669ed6c4abdce3be95c97ee3e276
jinwoo kim <jinwoo960323@likelion.org>
2021-07-30 11:22:04 +0900

    7/30 3 class commit 3

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,5 +1,9 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+app_name = 'articleapp'
 
 urlpatterns = [
-
+    path('list/', TemplateView.as_view(template_name='articaleapp/list.html'), name='list')
 
 ]
