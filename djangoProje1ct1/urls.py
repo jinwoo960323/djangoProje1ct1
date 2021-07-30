7e430b6c83d11aa906cd9a974180f686f139d8ba
jinwoo kim <jinwoo960323@likelion.org>
2021-07-30 10:23:26 +0900

    7/30 3 class commit 3

--- djangoProje1ct1/urls.py
+++ djangoProje1ct1/urls.py
@@ -2,8 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
+    path('articles/', include('articleapp.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
