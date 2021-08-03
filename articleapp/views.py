4a99a02019d7892b3c0a21d1b498befb02b98f8f
jinwoo kim <jinwoo960323@likelion.org>
2021-08-03 10:50:52 +0900

    8/3 3 class commit 2

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from articleapp.models import Article
+from articleapp.templates.forms import ArticleCreationForm
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    form_class = ArticleCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'articleapp/create.html'
+
+    def form_valid(self, form):
+        form.instance.writer = self.request.user
+        return super().form_valid(form)
